37615792900b201f1a28e7194f1d5564eae943a0
proofofjogi <ingokonito@protonmail.com>
2024-08-02 14:02:49 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# INSTALL SVELTE
+pnpm create svelte@latest frontend
+cd frontend
+
+pnpm install
+
+# install tailwindCSS
+pnpm install -D tailwindcss postcss autoprefixer
+pnpx tailwindcss init -p
+
+# install daisyUI
+pnpm i -D daisyui@latest
+
+# make tailwind files
+rm svelte.config.js
+
+# prepare svelte config file
+cat <<EOL > svelte.config.js
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
+export default config;
+EOL
+
+# prepare tailwind config file
+cat <<EOL > tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
+EOL
+
+# create CSS file
+cat <<EOL > ./src/app.css
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+EOL
+
+# create layout file
+cat <<EOL > ./src/routes/+layout.svelte
+<script>
+  import "../app.css";
+</script>
+
+<slot />
+EOL
+
+
+
+# fire VSCode
+code .
